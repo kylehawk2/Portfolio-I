13810b5afa915e6a49f816493888496a07e9ff2d
Kyle Hawkins <kylehawk2@gmail.com>
2018-09-19 18:38:40 -0700

    First Commit

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public string Index()
+        {
+            return "This is my Index!";
+        }
+        [HttpGet]
+        [Route("projects")]
+        public string Projects()
+        {
+            return "These are my projects!";
+        }
+        [HttpGet]
+        [Route("contact")]
+        public string Contact()
+        {
+            return "This is my Contact!";
+        }
+    }
+}
